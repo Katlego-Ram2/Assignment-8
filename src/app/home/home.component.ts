981764f1595047f5ba8042311f06bd041dcc023c
katlego.rampedi <katlego.rampedi@Ekurhuleni.gov.za>
2023-04-22 12:30:08 +0200

    forth commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  users: string[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
+    const storedUsers = JSON.parse(localStorage.getItem('users') || '[]');
+    this.users = storedUsers;
   }
 
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,11 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  router: any;
+
   email: string = '';
   password: string = '';
   errorMessage: string = '';
-  constructor() { }
+  constructor(private router: Router) {}
+ 
 
   ngOnInit(): void {
   }
